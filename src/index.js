b86a82b35b6e0b33a63d15f9a07b8468fc7f0828
miguelsalvadormangampo <miguelsalvadormangampo@gmail.com>
2025-09-11 00:48:43 +0800

    add express

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+import express from "express";
 import cron from "node-cron";
 import { startSync } from './bdo.js';
+
+const app = express();
 
 cron.schedule("0 */3 * * *", async () => {
   await startSync();
 });
+
+app.get("/", (req, res) => res.send("Asset miner is running"));
+app.listen(3000, () => console.log("App started on port 3000"));
