7a2132aad00978236d8b9213c9edd77f250c650d
miguelsalvadormangampo <miguelsalvadormangampo@gmail.com>
2025-10-11 06:19:24 +0800

    Include South Luzon in Security bank miner

--- src/security.js
+++ src/security.js
@@ -21,6 +21,11 @@ const gotoProps = {
   timeout: 120000,
 };
 
+const REGIONS = [
+  'National Capital Region',
+  'South Luzon',
+];
+
 const getDetails = async () => {
   const { browser, page } = await initBrowserPage();
   let details = null;
@@ -29,65 +34,69 @@ const getDetails = async () => {
     await page.waitForSelector('.property-table'); // wait for main list
     await delay(2000);
 
-    details = await page.evaluate(async ({ Residential, Commercial, Condominium, Townhouse }) => {
+    details = await page.evaluate(async ({ Residential, Commercial, Condominium, Townhouse, REGIONS }) => {
       const sanitize = text => (text || '').replaceAll('\n', '').replaceAll('\t', '');
       const delayFunc = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-      const locations = Array.from(
-        document.querySelector('optgroup[label="National Capital Region"]').querySelectorAll('option')
-      ).map(node => node.value);
+
       const list = [];
-      for (const loc of locations) {
-        const rows = document.querySelectorAll(`tr[data-regions="${loc}"]`);
+      for (const region of REGIONS) {
+        const locations = Array.from(
+          document.querySelector(`optgroup[label="${region}"]`).querySelectorAll('option')
+        ).map(node => node.value);
 
-        await delayFunc(1000);
+        for (const loc of locations) {
+          const rows = document.querySelectorAll(`tr[data-regions="${loc}"]`);
 
-        for (const row of rows) {
-          const cells = row.querySelectorAll('td');
-          const aTags = row.querySelectorAll('a');
-          const presyo = Number(sanitize(cells[4].textContent).replaceAll(',', ''));
-          const lotA = Number(sanitize(cells[2].textContent).split(' ')[0]);
-          const floorA = Number(sanitize(cells[3].textContent).split(' ')[0]);
+          await delayFunc(1000);
 
-          let thumbnail_url, link;
-          for (const a of aTags) {
-            if (a.textContent === 'Inquire') link = a.href;
-            if (a.textContent === 'Image') thumbnail_url = a.href;
+          for (const row of rows) {
+            const cells = row.querySelectorAll('td');
+            const aTags = row.querySelectorAll('a');
+            const presyo = Number(sanitize(cells[4].textContent).replaceAll(',', ''));
+            const lotA = Number(sanitize(cells[2].textContent).split(' ')[0]);
+            const floorA = Number(sanitize(cells[3].textContent).split(' ')[0]);
+
+            let thumbnail_url, link;
+            for (const a of aTags) {
+              if (a.textContent === 'Inquire') link = a.href;
+              if (a.textContent === 'Image') thumbnail_url = a.href;
+            }
+            if (!link) link = thumbnail_url;
+
+            let type = 'Others'
+            let property_type = 'Others';
+            const floor_area = !Number.isNaN(floorA) ? floorA : 0;
+            const lot_area = !Number.isNaN(lotA) ? lotA : 0;;
+            const location = sanitize(cells[0].textContent);
+            const address = sanitize(cells[1].textContent);
+            const price = !Number.isNaN(presyo) ? presyo : 0;
+
+            if ((address || '').includes(Residential)) type = Residential;
+            else if ((address || '').includes(Commercial)) type = Commercial;
+
+            if ((address || '').includes(Condominium)) property_type = Condominium;
+            else if ((address || '').includes(Townhouse)) property_type = Townhouse;
+
+            const sepText = address.split('(');
+            const description = sepText.length > 2 ? sepText[2].replaceAll(')', '') : '';
+            
+            list.push({
+              link,
+              thumbnail_url,
+              type,
+              property_type,
+              floor_area,
+              lot_area,
+              location,
+              address,
+              price,
+              description,
+            });
           }
-          if (!link) link = thumbnail_url;
-
-          let type = 'Others'
-          let property_type = 'Others';
-          const floor_area = !Number.isNaN(floorA) ? floorA : 0;
-          const lot_area = !Number.isNaN(lotA) ? lotA : 0;;
-          const location = sanitize(cells[0].textContent);
-          const address = sanitize(cells[1].textContent);
-          const price = !Number.isNaN(presyo) ? presyo : 0;
-
-          if ((address || '').includes(Residential)) type = Residential;
-          else if ((address || '').includes(Commercial)) type = Commercial;
-
-          if ((address || '').includes(Condominium)) property_type = Condominium;
-          else if ((address || '').includes(Townhouse)) property_type = Townhouse;
-
-          const sepText = address.split('(');
-          const description = sepText.length > 2 ? sepText[2].replaceAll(')', '') : '';
-          
-          list.push({
-            link,
-            thumbnail_url,
-            type,
-            property_type,
-            floor_area,
-            lot_area,
-            location,
-            address,
-            price,
-            description,
-          });
         }
       }
       return list;
-    }, { Residential, Commercial, Condominium, Townhouse });
+    }, { Residential, Commercial, Condominium, Townhouse, REGIONS });
 
     console.log(`${logTemplate}Data count: `, details.length)
 
